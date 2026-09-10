a077d737b3a359e317c70df4145bf72164890ff1
Marcos José Rivera Souto <mjrivera@knowmadmood.com>
2026-09-10 15:54:39 +0200

    refactor:  HomeComponent utilizando los selectores creados antes

--- src/app/courses/home/home.component.ts
+++ src/app/courses/home/home.component.ts
@@ -1,81 +1,60 @@
-import {Component, OnInit} from '@angular/core';
-import {compareCourses, Course} from '../model/course';
-import {Observable} from "rxjs";
-import {defaultDialogConfig} from '../shared/default-dialog-config';
-import {EditCourseDialogComponent} from '../edit-course-dialog/edit-course-dialog.component';
-import { MatDialog } from '@angular/material/dialog';
-import {map, shareReplay} from 'rxjs/operators';
-import {CoursesHttpService} from '../services/courses-http.service';
-
-
+import { Component, inject, OnInit } from "@angular/core";
+import { compareCourses, Course } from "../model/course";
+import { Observable } from "rxjs";
+import { defaultDialogConfig } from "../shared/default-dialog-config";
+import { EditCourseDialogComponent } from "../edit-course-dialog/edit-course-dialog.component";
+import { MatDialog } from "@angular/material/dialog";
+import { map, shareReplay, tap } from "rxjs/operators";
+import { CoursesHttpService } from "../services/courses-http.service";
+import { Store } from "@ngrx/store";
+import { AppState } from "../../reducers";
+import { selectAdvancedCourses, selectAllCourses, selectBeginnerCourses, selectPromoTotal } from "../courses.selectors";
 
 @Component({
-    selector: 'home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css'],
-    standalone: false
+  selector: "home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
+  standalone: false,
 })
 export class HomeComponent implements OnInit {
+  promoTotal$?: Observable<number>;
 
-    promoTotal$: Observable<number>;
+  loading$?: Observable<boolean>;
 
-    loading$: Observable<boolean>;
+  beginnerCourses$?: Observable<Course[]>;
 
-    beginnerCourses$: Observable<Course[]>;
+  advancedCourses$?: Observable<Course[]>;
 
-    advancedCourses$: Observable<Course[]>;
+  store = inject(Store<AppState>);
+  courses$ = this.store.select(selectAllCourses).pipe(
+    map((entities) => (Object.values(entities) || []).sort(compareCourses)),
+    shareReplay(), // Sino se usara shareReplay, cada vez que se suscriba un observable, se haría una nueva petición HTTP (en este caso 4 veces, por: loading, beginnerCourses, advancedCourses, promoTotal)
+  );
 
+  constructor(
+    private dialog: MatDialog,
+    private coursesHttpService: CoursesHttpService,
+  ) {}
 
-    constructor(
-      private dialog: MatDialog,
-      private coursesHttpService: CoursesHttpService) {
-
-    }
-
-    ngOnInit() {
-      this.reload();
-    }
+  ngOnInit() {
+    this.reload();
+  }
 
   reload() {
-
-    const courses$ = this.coursesHttpService.findAllCourses()
-      .pipe(
-        map(courses => courses.sort(compareCourses)),
-        shareReplay() // Sino se usara shareReplay, cada vez que se suscriba un observable, se haría una nueva petición HTTP (en este caso 4 veces, por: loading, beginnerCourses, advancedCourses, promoTotal)
-      );
-
-    this.loading$ = courses$.pipe(map(courses => !!courses));
-
-    this.beginnerCourses$ = courses$
-      .pipe(
-        map(courses => courses.filter(course => course.category == 'BEGINNER'))
-      );
-
-
-    this.advancedCourses$ = courses$
-      .pipe(
-        map(courses => courses.filter(course => course.category == 'ADVANCED'))
-      );
-
-    this.promoTotal$ = courses$
-        .pipe(
-            map(courses => courses.filter(course => course.promo).length)
-        );
-
+    this.courses$ = this.store.select(selectAllCourses);
+    this.promoTotal$ = this.store.select(selectPromoTotal);
+    this.beginnerCourses$ = this.store.select(selectBeginnerCourses);
+    this.advancedCourses$ = this.store.select(selectAdvancedCourses);
   }
 
   onAddCourse() {
-
     const dialogConfig = defaultDialogConfig();
 
     dialogConfig.data = {
-      dialogTitle:"Create Course",
-      mode: 'create'
+      dialogTitle: "Create Course",
+      mode: "create",
     };
 
     this.dialog.open(EditCourseDialogComponent, dialogConfig);
-
   }
-
-
 }
